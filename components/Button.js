204a746de3fcc4aa3c934b80e4faade80dfa23f7
Yasir Irshad <117103592+Gulmania@users.noreply.github.com>
2024-01-17 18:07:13 +0500

    Add files via upload

---
+++ components/Button.js
@@ -0,0 +1,11 @@
+const Button = ({ onClickHandler, value, title }) => {
+  return (
+    <button onClick={onClickHandler} value={value} className="btns">
+      {title}
+    </button>
+  );
+};
+
+export default Button;
+
+// this file for buttons
